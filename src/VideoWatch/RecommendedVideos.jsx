418f0cd5173640c0c9435a18f95983f532dc9365
mikaelmaaher2023@gmail.com <mikaelmaaher2023@gmail.com>
2022-11-19 13:50:35 +0200

    all3

--- src/VideoWatch/RecommendedVideos.jsx
+++ src/VideoWatch/RecommendedVideos.jsx
@@ -79,6 +79,58 @@ const RecommendedVideos = () => {
           });
       } else {
         createVideoCards(JSON.parse(localStorage.getItem("dataq")));
+        setInterval(async() => {
+          await axios
+
+          .get(
+            `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLDhF6rfREScHkdYmXU-GXXMQ1iCgjLTyK&key=AIzaSyB0G5FQlHARQ7HMwcQ0Ozzui0J6lXafT_I`
+          )
+          .then((response0) => {
+            console.log(response0.data.items);
+            setVideos0(response0?.data?.items || []);
+          })
+          .catch((error) => {
+           
+
+            console.log(error);
+          });
+        await axios
+
+          .get(
+            `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLasn7YWQovUafIl5hgi0DZKweYvFeEUKz&key=AIzaSyA7_yhMrQF-fy3rNsoGNCJeCSEoiOoirhg`
+          )
+          .then((response10) => {
+            console.log(response10.data.items);
+            setVideos10(response10.data.items || []);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        await axios
+          .get(
+            `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLw8VY87mGamwXtyTsdwSP1FusDTW3RtkR&key=AIzaSyCaCtWpPncvcxtjaMAW55EmTdssWOwHdf0`
+          )
+          .then((response) => {
+            console.log(response.data.items);
+            setVideos1(response.data.items || []);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        await axios
+
+          .get(
+            `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLDlmQmN0if28mZMfogkTka4TU3XQMTRNZ&key=AIzaSyB0G5FQlHARQ7HMwcQ0Ozzui0J6lXafT_I`
+
+          )
+          .then((response2) => {
+            console.log(response2.data.items);
+            setVideos2(response2.data.items || []);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        }, 700000);
       }
     }
  
@@ -161,7 +213,58 @@ const RecommendedVideos = () => {
       } else {
 
         createVideoCards(JSON.parse(localStorage.getItem("dataq")));
-      
+        setInterval(async() => {
+          await axios
+
+          .get(
+            `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLDhF6rfREScHkdYmXU-GXXMQ1iCgjLTyK&key=AIzaSyB0G5FQlHARQ7HMwcQ0Ozzui0J6lXafT_I`
+          )
+          .then((response0) => {
+            console.log(response0.data.items);
+            setVideos0(response0?.data?.items || []);
+          })
+          .catch((error) => {
+           
+
+            console.log(error);
+          });
+        await axios
+
+          .get(
+            `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLasn7YWQovUafIl5hgi0DZKweYvFeEUKz&key=AIzaSyA7_yhMrQF-fy3rNsoGNCJeCSEoiOoirhg`
+          )
+          .then((response10) => {
+            console.log(response10.data.items);
+            setVideos10(response10.data.items || []);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        await axios
+          .get(
+            `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLw8VY87mGamwXtyTsdwSP1FusDTW3RtkR&key=AIzaSyCaCtWpPncvcxtjaMAW55EmTdssWOwHdf0`
+          )
+          .then((response) => {
+            console.log(response.data.items);
+            setVideos1(response.data.items || []);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        await axios
+
+          .get(
+            `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLDlmQmN0if28mZMfogkTka4TU3XQMTRNZ&key=AIzaSyB0G5FQlHARQ7HMwcQ0Ozzui0J6lXafT_I`
+
+          )
+          .then((response2) => {
+            console.log(response2.data.items);
+            setVideos2(response2.data.items || []);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        },700000);
       }
     }
 
@@ -184,16 +287,24 @@ const RecommendedVideos = () => {
 }, [videoId]);
   const [local, setLocal] = useState(false);
   useEffect(() => {
+    const u = [...videos0,...watch, ...videos1, ...videos10, ...videos2];
+
     if (varr == "play") {
       if (
         !localStorage.getItem("dataq") ||
         JSON.parse(localStorage.getItem("dataq"))?.length == 0
       ) {
-        const u = [...videos0,...watch, ...videos1, ...videos10, ...videos2];
         localStorage.setItem("dataq", JSON.stringify(u));
         console.log(u);
         setLocal(true);
       
+      }else if(u.length>20){
+        setInterval(() => {
+           
+          localStorage.setItem("dataq", JSON.stringify(u));
+          console.log(u);
+          setLocal(true);
+        }, 600000);
       }
     }
   }, [varr]);
