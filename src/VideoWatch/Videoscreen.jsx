863948395850b0f7d28c932b716ab3822b8bca98
mikaelmaaher2023@gmail.com <mikaelmaaher2023@gmail.com>
2022-11-27 17:54:51 +0200

    all

--- src/VideoWatch/Videoscreen.jsx
+++ src/VideoWatch/Videoscreen.jsx
@@ -4,10 +4,185 @@ import SideBar from "./SideBar ";
 import axios from "axios";
 import VideoInfo from "./VideoInfo";
 import "./Videoscreen.css";
-import { useParams } from "react-router-dom";
+ 
 import aaa from "./aaa.gif"
+import {  useNavigate, useParams } from "react-router-dom";
+
+import {
+  getVideosFromYotube,
+  createVideoLists,
+  createVideosCards,
+  shuffleArray,
+} from "./functions.js";
 function Videoscreen() {
-  let { videoId } = useParams();
+  const { PlayListId, videoId } = useParams();
+
+  const [videoCards1, setVideoCards1] = useState([]);
+
+  {
+    /*useEffect(()=>{
+    setInterval(() => {
+      const MyAsyncFun = async () => {
+        const PlayList1 = await getVideosFromYotube(
+          "30",
+          "PLDhF6rfREScHkdYmXU-GXXMQ1iCgjLTyK",
+          "AIzaSyCaCtWpPncvcxtjaMAW55EmTdssWOwHdf0"
+        );
+        const PlayList2 = await getVideosFromYotube(
+          "30",
+          "PLDlmQmN0if28mZMfogkTka4TU3XQMTRNZ",
+          "AIzaSyB0G5FQlHARQ7HMwcQ0Ozzui0J6lXafT_I"
+        );
+        const PlayList3 = await getVideosFromYotube(
+          "30",
+          "PLasn7YWQovUafIl5hgi0DZKweYvFeEUKz",
+          "AIzaSyA7_yhMrQF-fy3rNsoGNCJeCSEoiOoirhg"
+        );
+        const ArrayofLists = createVideoLists(PlayList1);
+        const ArrayofLists2 = createVideoLists(PlayList2);
+        const ArrayofLists3 = createVideoLists(PlayList3);
+  
+        console.log(ArrayofLists);
+        const First = createVideosCards(ArrayofLists[0]);
+        const Second = createVideosCards(ArrayofLists[1]);
+        const First2 = createVideosCards(ArrayofLists2[0]);
+        const Second2 = createVideosCards(ArrayofLists2[1]);
+        const First3 = createVideosCards(ArrayofLists3[0]);
+        const Second3 = createVideosCards(ArrayofLists3[1]);
+        let Last = [
+          ...First,
+          ...Second,
+          ...First2,
+          ...Second2,
+          ...First3,
+          ...Second3,
+        ];
+        Last = shuffleArray(Last);
+        localStorage.setItem("data", JSON.stringify(Last));
+        setVideoCards1(Last);
+      };
+      MyAsyncFun()
+      setIsLoading(false)
+    }, 1000101);
+  },[videoId])*/
+  }
+  {
+    /*
+useEffect(() => {
+    setIsLoading(true)
+    let Nosavedwatch =
+      !localStorage.getItem("data") ||
+      JSON.parse(localStorage.getItem("data"))?.length == 0;
+    const body = document.querySelector("#root");
+    body.scrollIntoView({}, 200);
+    const MyAsyncFun = async () => {
+      const PlayList1 = await getVideosFromYotube(
+        "30",
+        "PLDhF6rfREScHkdYmXU-GXXMQ1iCgjLTyK",
+        "AIzaSyCaCtWpPncvcxtjaMAW55EmTdssWOwHdf0"
+      );
+      const PlayList2 = await getVideosFromYotube(
+        "30",
+        "PLDlmQmN0if28mZMfogkTka4TU3XQMTRNZ",
+        "AIzaSyB0G5FQlHARQ7HMwcQ0Ozzui0J6lXafT_I"
+      );
+      const PlayList3 = await getVideosFromYotube(
+        "30",
+        "PLasn7YWQovUafIl5hgi0DZKweYvFeEUKz",
+        "AIzaSyA7_yhMrQF-fy3rNsoGNCJeCSEoiOoirhg"
+      );
+      const ArrayofLists = createVideoLists(PlayList1);
+      const ArrayofLists2 = createVideoLists(PlayList2);
+      const ArrayofLists3 = createVideoLists(PlayList3);
+
+      console.log(ArrayofLists);
+      const First = createVideosCards(ArrayofLists[0]);
+      const Second = createVideosCards(ArrayofLists[1]);
+      const First2 = createVideosCards(ArrayofLists2[0]);
+      const Second2 = createVideosCards(ArrayofLists2[1]);
+      const First3 = createVideosCards(ArrayofLists3[0]);
+      const Second3 = createVideosCards(ArrayofLists3[1]);
+      let Last = [
+        ...First,
+        ...Second,
+        ...First2,
+        ...Second2,
+        ...First3,
+        ...Second3,
+      ];
+      Last = shuffleArray(Last);
+      localStorage.setItem("data", JSON.stringify(Last));
+      setVideoCards1(Last);
+    };
+    if (Nosavedwatch) {
+      MyAsyncFun();
+    } else {
+      const data = JSON.parse(localStorage.getItem("data"));
+      const shuflled = shuffleArray(data);
+      setVideoCards1(shuflled);
+    }
+    setIsLoading(false);
+  }, [videoId]);
+
+*/
+  }
+  const navigate = useNavigate();
+  useEffect(() => {
+    setIsLoading(true);
+    const data = JSON.parse(localStorage.getItem("data"));
+    const shuflled = shuffleArray(data);
+    setVideoCards1(shuflled); 
+    const body = document.querySelector("#root");
+    body.scrollIntoView();
+    setIsLoading(false)
+    setTimeout(() => {
+     setGIF(false)
+    },900);
+  
+  
+ 
+   
+  }, [videoId]);
+  useEffect(() => {
+    setIsLoading(true);
+    let Nosavedwatch =
+      !localStorage.getItem("data") ||
+      JSON.parse(localStorage.getItem("data"))?.length == 0;
+    const body = document.querySelector("#root");
+    body.scrollIntoView({}, 200);
+    console.log(PlayListId);
+    const MyAsyncFun = async () => {
+      const PlayList1 = await getVideosFromYotube(
+        "50",
+        PlayListId,
+        "AIzaSyCaCtWpPncvcxtjaMAW55EmTdssWOwHdf0"
+      );
+
+      const ArrayofLists = createVideoLists(PlayList1);
+
+      console.log(ArrayofLists);
+      const MyList = createVideosCards(PlayList1);
+
+      let Last = shuffleArray(MyList);
+      localStorage.setItem("data", JSON.stringify(Last));
+      setVideoCards1(Last);
+    };
+
+    MyAsyncFun();
+
+    setIsLoading(false)
+    setTimeout(() => {
+     setGIF(false)
+    },900);
+
+   
+  }, [PlayListId]);
+
+
+
+
+
+
 
   const [videoInfo, setVideoInfo] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -67,9 +242,7 @@ const [gif,setGIF]=useState(true);
       subs,
     }); 
     setIsLoading(false)
-    setTimeout(() => {
-     setGIF(false)
-    },900);
+
     
   }
   return (<div>
@@ -83,10 +256,9 @@ const [gif,setGIF]=useState(true);
       <div>
         <iframe
           className="myvideo2"
-          src={`https://www.youtube.com/embed/${videoId}`}
-          title="YouTube player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media;   picture-in-picture"
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+          allow='autoplay'
+         
           allowFullScreen
         ></iframe>
         <div className="videoplayer__videoinfo">
@@ -107,7 +279,7 @@ const [gif,setGIF]=useState(true);
       </div>
       <div>
         <div className="videoplayer__suggested">
-          <RecommendedVideos/>
+          <RecommendedVideos videoCards1={videoCards1}  isLoading={isLoading}/>
         </div>
       </div>
     </div>
