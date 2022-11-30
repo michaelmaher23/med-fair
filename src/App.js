2223285b5476b27b1f8e0935f50569fbb16c6c89
michael <mikaelmaaher2023@gmail.com>
2022-11-30 08:42:54 +0000

    'all'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Index from "./pages/Home/Index.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Contactus from "./pages/Contactus/Contactus.jsx";
 import Footer from "./MYFOOTER/Footer.jsx";
@@ -8,6 +8,7 @@ import Navbar from "./VideoWatch/Sidebar2.jsx";
 import Aboutus from "./pages/Contactus/Aboutus.jsx";
 import SideBox from "./VideoTexts.jsx/SideBox.jsx";
 import Watch from "./VideoWatch/Watch.jsx";
+import "./App.css";
 function App() {
   return (
     < >
